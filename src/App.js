908cd8b2de677877e944230d91508a4693650c74
theodorejohn7 <theodore.john@10decoders.in>
2024-05-24 11:01:15 +0530

    web socket

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, useSubscription, gql } from '@apollo/client';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, useSubscription, gql, HttpLink, split } from '@apollo/client';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: 'https://gmeet-node-ls52.vercel.app/graphql',
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'wss://gmeet-node-ls52.vercel.app/graphql',
+  connectionParams: {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  },
+}));
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  link: new GraphQLWsLink(createClient({
-    url: 'ws://gmeet-node-ls52.vercel.app/graphql',
-  })),
-  cache: new InMemoryCache()
+  link: splitLink,
+  cache: new InMemoryCache(),
 });
 
 const PARTICIPANTS_QUERY = gql`
@@ -30,7 +54,7 @@ function Participants() {
 
   return (
     <div className="App">
-      <h1>Google Meet Participants -v2.1</h1>
+      <h1>Google Meet Participants -v2.2</h1>
       <ul>
         {participants.map((participant, index) => (
           <li key={index}>{participant}</li>
